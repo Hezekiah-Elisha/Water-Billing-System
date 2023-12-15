6a9481339b58e210f063a856da125643913e7b7a
Hezekiah-Elisha <elishahezekiah903@gmail.com>
2023-12-15 16:11:07 +0300

    :construction: Add Theme

--- water-billing-system/src/main.js
+++ water-billing-system/src/main.js
@@ -9,7 +9,8 @@ import 'bootstrap-icons/font/bootstrap-icons.css'
 import axios from 'axios'
 
 const pinia = createPinia()
-axios.defaults.baseURL = 'http://localhost:7000';
+// axios.defaults.baseURL = 'http://localhost:7000';
+axios.defaults.baseURL = 'https://hezekiahelisha.pythonanywhere.com/';
 axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('token');
 
 
